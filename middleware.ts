3c74c3874a8cfab3951436c7d9759a3748e423a6
Niki704 <chamupathikarunaweera@gmail.com>
2026-09-21 12:03:48 +0530

    feat(fix): Revert the changes applied to the i18n import path.

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 import createMiddleware from "next-intl/middleware";
-import { locales, defaultLocale } from "./i18n.config.js";
+import { locales, defaultLocale } from "./i18n.config";
 import { NextRequest } from "next/server";
 
 const intlMiddleware = createMiddleware({
